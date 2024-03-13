bd1e8449adf9155cc2e5a7f843784ec21ccf156a
NibinByteCode <nibinm365@gmail.com>
2024-03-13 17:53:32 -0400

    Add post recycler

--- app/src/main/java/com/example/jobscan/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/jobscan/ui/home/HomeFragment.kt
@@ -6,17 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jobscan.adapters.HomeRecyclerAdapter
 import com.example.jobscan.databinding.FragmentHomeBinding
+import com.example.jobscan.models.PostData
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.FirebaseDatabase
 
 class HomeFragment : Fragment() {
-
+    private var adapter: HomeRecyclerAdapter? = null
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -26,25 +28,19 @@ class HomeFragment : Fragment() {
     ): View {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
-
+        Log.i("data","inside")
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        val searchView : SearchView = binding.searchView
-
-        // Set up a query listener to handle search query submission
+        val rView: RecyclerView = binding.postRecycler
+        val searchView: SearchView = binding.searchView
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
-                Log.i("test","Search done")
+                Log.i("test", "Search done")
                 return true
             }
 
             override fun onQueryTextChange(newText: String): Boolean {
-                Log.i("test","Search done")
+                Log.i("test", "Search done")
                 return true
             }
 
@@ -52,6 +48,27 @@ class HomeFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val query = FirebaseDatabase.getInstance().reference.child("Posts")
+        val options = FirebaseRecyclerOptions.Builder<PostData>()
+            .setQuery(query, PostData::class.java)
+            .build()
+        Log.i("data","getting data")
+        adapter = HomeRecyclerAdapter(options)
+        binding.postRecycler.layoutManager = LinearLayoutManager(activity)
+        binding.postRecycler.adapter = adapter
+
+    }
+    override fun onStart() {
+        super.onStart()
+        adapter?.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter?.stopListening()
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
