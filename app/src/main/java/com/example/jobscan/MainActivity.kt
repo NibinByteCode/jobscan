26141f2b5373b62d5fa9013272bc519aeec52fea
NibinByteCode <nibinm365@gmail.com>
2024-03-14 18:04:38 -0400

    Home Post Recycler

--- app/src/main/java/com/example/jobscan/MainActivity.kt
+++ app/src/main/java/com/example/jobscan/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.jobscan
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.SearchView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.jobscan.CandidateActivity
 import com.example.jobscan.DetailActivity
+import com.example.jobscan.adapters.HomeRecyclerAdapter
+import com.example.jobscan.models.PostData
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    private var adapter: HomeRecyclerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +26,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val rView: RecyclerView = findViewById(R.id.postRecycler)
+        val searchView: SearchView = findViewById(R.id.searchView)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Log.i("test", "Search done")
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                Log.i("test", "Search done")
+                return true
+            }
+
+        })
+        val query = FirebaseDatabase.getInstance().reference.child("Posts")
+        val options = FirebaseRecyclerOptions.Builder<PostData>()
+            .setQuery(query, PostData::class.java)
+            .build()
+        Log.i("data","getting data")
+        adapter = HomeRecyclerAdapter(options)
+        rView.layoutManager = LinearLayoutManager(this)
+        rView.adapter = adapter
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter?.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter?.stopListening()
     }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
